534543d621847782e1b64be721882b818089aa06
Denis Golovan <denisgolovan@yandex.ru>
2017-01-21 08:55:04 +0200

    Parser-time "int list->int vec" specialization

--- src/ast.rs
+++ src/ast.rs
@@ -37,3 +37,51 @@ impl<'a> Arena<'a> {
         unsafe { &*p }
     }
 }
+
+fn is_monovec<'a>(n:&'a Vec<ASTNode<'a>>) -> bool {
+    let isvec = n.iter().all( |x| match x {
+        &ASTNode::Node(AST::Value(Value::Number(_))) => true,
+        _ => false
+    });
+    isvec
+}
+
+fn to_monovec<'a,'b>(n:&'a Vec<ASTNode<'a>>) -> AST<'b> {
+    let mut r:Vec<i32> = Vec::new();
+
+    for v in n.iter() {
+      match v {
+         &ASTNode::Node(AST::Value(Value::Number(x))) => { r.push(x); },
+         _ => panic!("Unexpected non-number")
+      }
+    }
+    AST::Value(Value::VecInt(r))
+}
+
+pub fn postprocess<'a,'b>(n:&'b ASTNode<'a>, arena: &'a Arena<'a>) -> ASTNode<'a> {
+    match n {
+      &ASTNode::Node(ref x) => {
+        ASTNode::Node(
+          match x {
+            &AST::Verb(ref a, ref b) => {
+               AST::Verb( arena.alloc( postprocess(a, arena) ), arena.alloc( postprocess(b, arena) ) )
+            },
+            &AST::Value(ref x) => {
+               AST::Value( x.clone() )
+            },
+            &AST::List(ref x) => {
+               AST::List( arena.alloc( postprocess(x, arena) ) )
+            }
+          })
+      },
+      &ASTNode::Vec(ref x) => {
+        if is_monovec(x) { 
+            ASTNode::Node(to_monovec(x))
+        } else { 
+            let v = x.iter().map(|x| postprocess(x, arena) ).collect();
+            ASTNode::Vec( v )
+        }
+      },
+      x => x.clone()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -27,3 +27,21 @@ fn test1() {
                 ]))
         ));
 }
+
+#[test]
+fn test2() {
+    let arena = Arena::new();
+    let ast = parser::parse_Expression(&arena, &"(1+1;1;(1;1;1))".to_string()).unwrap();
+    let p_ast = postprocess(&ast, &arena);
+    
+    assert_eq!( p_ast,
+        ASTNode::Node(AST::List(
+            &ASTNode::Vec(vec![
+                ASTNode::Node( AST::Verb( 
+                                 &ASTNode::Node(AST::Value(Value::Number(1))),
+                                 &ASTNode::Node(AST::Value(Value::Number(1))) )),
+                ASTNode::Node( AST::Value(Value::Number(1))),
+                ASTNode::Node( AST::List( &ASTNode::Node( AST::Value(Value::VecInt( vec![1,1,1])) )))
+                ]))
+        ));
+}
